b008c712abe817e746135b49b4b4b31b861e350a
KhalidGad <khaled.626528@gmail.com>
2023-04-11 23:52:34 +0200

    Add challenge 3 solution

---
+++ challenge-3/run.sh
@@ -0,0 +1,25 @@
+kubectl apply -f manifests/vote-namespace.yml
+
+sleep 2
+
+kubectl apply -f manifests/vote-deployment.yml
+kubectl apply -f manifests/vote-service.yml
+
+sleep 2
+
+kubectl apply -f manifests/redis-deployment.yml
+kubectl apply -f manifests/redis-service.yml
+
+sleep 2
+
+kubectl apply -f manifests/worker-deployment.yml
+
+sleep 2
+
+kubectl apply -f manifests/db-deployment.yml
+kubectl apply -f manifests/db-service.yml
+
+sleep 2
+
+kubectl apply -f manifests/result-deployment.yml
+kubectl apply -f manifests/result-service.yml
